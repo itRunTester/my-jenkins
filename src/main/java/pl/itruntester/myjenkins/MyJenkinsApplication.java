04d897a8fa71f105a26eb3442e6d369109a38ca8
itRunTester <itrun.tester@gmail.com>
2020-09-20 22:59:02 +0200

    Push

---
+++ src/main/java/pl/itruntester/myjenkins/MyJenkinsApplication.java
@@ -0,0 +1,21 @@
+package pl.itruntester.myjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJenkinsApplication.class, args);
+    }
+
+    @GetMapping
+    public String get() {
+        return "owyeah Piorun!";
+    }
+
+}
