59725f5cbdcaacc8f36b747a2d1ee719e1180016
itRunTester <itrun.tester@gmail.com>
2020-09-24 22:13:30 +0200

    Get

--- src/test/java/pl/itruntester/myjenkins/MyJenkinsApplicationTests.java
+++ src/test/java/pl/itruntester/myjenkins/MyJenkinsApplicationTests.java
@@ -2,16 +2,10 @@ package pl.itruntester.myjenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -26,7 +20,7 @@ class MyJenkinsApplicationTests {
 
     @Test
     void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("owyeah Piorun!"));
