7a470244ab00886f89d471a6ba36e32fa548ae2a
itRunTester <itrun.tester@gmail.com>
2020-09-24 21:18:48 +0200

    Push

--- src/test/java/pl/itruntester/myjenkins/MyJenkinsApplicationTests.java
+++ src/test/java/pl/itruntester/myjenkins/MyJenkinsApplicationTests.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -25,8 +27,9 @@ public class MyJenkinsApplicationTests {
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
         assert false;
-        ResultActions resultActions = this.mockMvc.perform(get()).andDo(print()).andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().string("owyeah Piorun!"));
+        ResultActions resultActions;
+        resultActions = this.mockMvc.perform(get()).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("owyeah Piorun!"));
     }
     private RequestBuilder get() {
         String s = "owyeah Piorun!";
